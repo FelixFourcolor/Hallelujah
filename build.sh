36fc91bf7baa4c8d9ffaf9f8872c0f600e89f812
Felix <felix.fourcolor@gmail.com>
2023-11-30 13:37:14 -0800

    init

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+branch=$(git branch --show-current)
+
+git checkout main &>/dev/null
+git checkout -b integration &>/dev/null
+git merge instrumentation --no-edit &>/dev/null
+git merge phrasing --no-edit &>/dev/null
+
+noteblock-generator src World --location 0 -60 0 --orientation 0 -90 --theme netherite_block
+
+git checkout $branch &>/dev/null
+git branch -D integration &>/dev/null
