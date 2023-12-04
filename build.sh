dfdc0dede5b58e406ff26b1626432122214878e7
Felix <felix.fourcolor@gmail.com>
2023-12-04 13:51:21 -0800

    free build output options

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ git checkout -b integration &>/dev/null
 git merge instrumentation --no-edit &>/dev/null
 git merge phrasing --no-edit &>/dev/null
 
-noteblock-generator src World --location 0 -60 0 --orientation 0 -90 --theme netherite_block -q $@
+noteblock-generator src World --location 0 -60 0 --orientation 0 -90 --theme netherite_block $@
 
 git checkout $branch &>/dev/null
 git branch -D integration &>/dev/null
