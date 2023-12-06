e5efc001c79843f522123148f3738f5897e26e16
Felix <felix.fourcolor@gmail.com>
2023-12-06 13:52:36 -0800

    tweak build

--- build.sh
+++ build.sh
@@ -2,12 +2,11 @@
 
 branch=$(git branch --show-current)
 
-git checkout main &>/dev/null
-git checkout -b integration &>/dev/null
-git merge instrumentation --no-edit &>/dev/null
-git merge phrasing --no-edit &>/dev/null
+git checkout -b integration >/dev/null
+git merge main --no-edit >/dev/null
+git merge instrumentation --no-edit >/dev/null
 
 noteblock-generator src World --location 0 -60 0 --orientation 0 -90 --theme netherite_block $@
 
-git checkout $branch &>/dev/null
-git branch -D integration &>/dev/null
+git checkout $branch >/dev/null
+git branch -D integration >/dev/null
